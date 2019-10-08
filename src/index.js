ddc43efb5eaf2aa0a6fc8d381d81f2d0dc72428b
Alexandra <Alexandra.kunitskaya@gmail.com>
2019-10-08 07:37:32 +0300

    init: start implementing task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let brackets = str.split('');
+
+    //check that all brackets are in config
+    let bracketInConfig = [];
+    brackets.forEach(bracket => {
+        bracketInConfig = bracketsConfig.filter(config => config.includes(bracket));
+        if (!bracketInConfig.length) return false;
+
+        const config = bracketsConfig.find(config => config.includes(bracket));
+        const openingBracketIndex = brackets.indexOf(config[0]);
+        let tempBrackets = brackets.slice(openingBracketIndex + 1);
+        brackets.splice(openingBracketIndex, 1);
+        const closingBracketIndex = brackets.indexOf(config[1]);
+        if (tempBrackets.includes(config[1])) brackets.splice(closingBracketIndex, 1);
+    });
+    return !brackets.length;
+};

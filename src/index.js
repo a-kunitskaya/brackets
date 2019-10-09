f4cb041bcaff76ba563b6971d37c4beb51635f57
Alexandra <Alexandra.kunitskaya@gmail.com>
2019-10-09 10:01:29 +0300

    feat: implement algorithm

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-    let brackets = str.split('');
+    const config = new Map(bracketsConfig);
+    const opening = new Set(config.keys());
+    const closing = new Set(config.values());
 
-    //check that all brackets are in config
-    let bracketInConfig = [];
-    brackets.forEach(bracket => {
-        bracketInConfig = bracketsConfig.filter(config => config.includes(bracket));
-        if (!bracketInConfig.length) return false;
-
-        const config = bracketsConfig.find(config => config.includes(bracket));
-        const openingBracketIndex = brackets.indexOf(config[0]);
-        let tempBrackets = brackets.slice(openingBracketIndex + 1);
-        brackets.splice(openingBracketIndex, 1);
-        const closingBracketIndex = brackets.indexOf(config[1]);
-        if (tempBrackets.includes(config[1])) brackets.splice(closingBracketIndex, 1);
-    });
-    return !brackets.length;
+    const temp = [];
+    for (let strBracket of str) {
+        if (opening.has(strBracket)) {
+            const matchingClosing = config.get(strBracket);
+            if (temp[temp.length - 1] === matchingClosing && matchingClosing === strBracket) {
+                temp.pop();
+            } else {
+                temp.push(strBracket);
+            }
+        } else if (closing.has(strBracket)) {
+            const lastBracket = temp[temp.length - 1];
+            if (config.get(lastBracket) === strBracket) {
+                temp.pop()
+            } else {
+                temp.push(strBracket);
+            }
+        }
+    }
+    return !temp.length;
 };
